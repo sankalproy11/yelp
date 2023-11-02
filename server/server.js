995acc24fbef7223e1908d93b66b0c970e8c9e05
sankalproy11 <sankalproy11@gmail.com>
2023-11-02 19:36:42 +0530

    fixed async await

--- server/server.js
+++ server/server.js
@@ -26,14 +26,23 @@ app.get("/api/v1/restaurants", async (req, res) => {
 });
 
 // get a restaurant
-app.get("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params);
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: "McDonald's",
-    },
-  });
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  console.log(req.params.id);
+
+  try {
+    const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [
+      req.params.id,
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // create a restaurant
