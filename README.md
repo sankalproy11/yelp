e92d18ea380ecc102598c4e8fdbecc428937d7d3
sankalproy11 <sankalproy11@gmail.com>
2023-11-02 17:07:09 +0530

    initial commit
    Signed-off-by: sankalproy11 <sankalproy11@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# YELP
+
+A simple user-generated online evaluations and ratings of restaurants, and services, helping people make informed decisions based on the experiences of others. App made using PERN Stack.
+
+![PERN](https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200402205611/What-is-PERN-Stack.png)
---
+++ server/server.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const express = require("express");
+const db = require("./db");
+
+const app = express();
+//const morgan = require("morgan");
+
+// middleware
+app.use(express.json());
+
+// get all restaurants
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM restaurants");
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurant: results.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// get a restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params);
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "McDonald's",
+    },
+  });
+});
+
+// create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      restaurant: "McDonald's",
+    },
+  });
+});
+
+// update a restaurant
+
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: "McDonald's",
+    },
+  });
+});
+// delete a restaurant
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
